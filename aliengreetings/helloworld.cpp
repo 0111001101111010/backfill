fe699ca160c0ed25cdb2aa365ff5fc15f3db0053
szhen002 <szhen002@Odu.edu>
2013-07-29 21:15:47 -0400

    gridding it

--- aliengreetings/helloworld.cpp
+++ aliengreetings/helloworld.cpp
@@ -2,7 +2,55 @@
 
 using namespace std;
 
-int main(int argc, char **argv){
+// build ascii squares/rhombuses
+	//verticle line fake sound bars generator  
+/*
 
+11*11
+1***1
+*****
+1***1
+11*11
+
+1111*
+111***
+11***** 
+111***
+1111*
+
+1111*
+111***
+11***** 
+1*******
+*********
+1*******
+11*****
+111***
+1111*
+
+
+*/
+int main(int argc, char **argv){
 cout << "big booty bitches\n";
-	return 0;}
+cout << "enter the cross length \n";
+
+int cross =0, times=0, iter =0;
+	
+	cin >> cross;
+	times = (cross/2)+1; // number of repetitions
+
+for (int i = 0; i < times; i++){
+	iter = cross-i; 
+	cout << "\n";
+	for (int j =0; j < times; j++){
+		//for (int k =0; cross 
+		int clone = iter;
+			//if ( iter >){
+				cout  << "*";
+			//}
+				cout << " ";
+	}
+}
+
+
+return 0;}
